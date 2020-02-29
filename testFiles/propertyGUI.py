39467095b035ad34f21542fffa6d7bd05e0bafc8
Dawson Schraiber <dschraiber1@gmail.com>
2020-02-29 09:58:24 -0600

    Separated Vision into own library

--- testFiles/propertyGUI.py
+++ testFiles/propertyGUI.py
@@ -1,21 +1,7 @@
 import pyglet
 import cv2 as cv
 import numpy as np
-
-vs = cv.VideoCapture(0)
-hsvLimitLower = np.array([70, 90, 60])  # (H,S,V) #243
-hsvLimitUpper = np.array([100, 255, 255])
-visionProperties = {"areaMin": 3,
-                    "widthMin": 10,
-                    "widthMax": 1003,
-                    "heightMin": 2,
-                    "heightMax": 1003,
-                    "vertexMin": 0,
-                    "vertexMax": 1000000,
-                    "ratioMin": 1,
-                    "ratioMax": 1000,
-                    "solidityMin": 5,
-                    "solidityMax": 48}
+import visionLibrary as vl
 
 
 class Rectangle(object):
@@ -75,9 +61,6 @@ class Button(object):
 
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
-        global hsvLimitLower
-        global hsvLimitUpper
-        global visionProperties
         super(Window, self).__init__(400, 720, caption='Vision Settings')
 
         self.batch = pyglet.graphics.Batch()
@@ -87,15 +70,15 @@ class Window(pyglet.window.Window):
             pyglet.text.Label('HSV UPPERLIMIT', x=10, y=640, anchor_y='bottom',
                               color=(0, 0, 0, 255), batch=self.batch)]
         self.widgets = [
-            TextWidget(str(hsvLimitLower[0]), 200, 700, 30, self.batch),
-            TextWidget(str(hsvLimitLower[1]), 240, 700, 30, self.batch),
-            TextWidget(str(hsvLimitLower[2]), 280, 700, 30, self.batch),
-            TextWidget(str(hsvLimitUpper[0]), 200, 640, 30, self.batch),
-            TextWidget(str(hsvLimitUpper[1]), 240, 640, 30, self.batch),
-            TextWidget(str(hsvLimitUpper[2]), 280, 640, 30, self.batch)]
+            TextWidget(str(vl.hsvLimitLower[0]), 200, 700, 30, self.batch),
+            TextWidget(str(vl.hsvLimitLower[1]), 240, 700, 30, self.batch),
+            TextWidget(str(vl.hsvLimitLower[2]), 280, 700, 30, self.batch),
+            TextWidget(str(vl.hsvLimitUpper[0]), 200, 640, 30, self.batch),
+            TextWidget(str(vl.hsvLimitUpper[1]), 240, 640, 30, self.batch),
+            TextWidget(str(vl.hsvLimitUpper[2]), 280, 640, 30, self.batch)]
 
-        for prop in visionProperties:
-            self.widgets.append(TextWidget(str(visionProperties[prop]), 200,
+        for prop in vl.visionProperties:
+            self.widgets.append(TextWidget(str(vl.visionProperties[prop]), 200,
                                            640 - 40*(len(self.widgets) - 5),
                                            60, self.batch))
             self.labels.append(pyglet.text.Label(prop, x=10,
@@ -163,9 +146,6 @@ class Window(pyglet.window.Window):
             self.focus.caret.on_text_motion_select(motion)
 
     def on_key_press(self, symbol, modifiers):
-        global hsvLimitLower
-        global hsvLimitUpper
-        global visionProperties
         if symbol == pyglet.window.key.TAB:
             if modifiers & pyglet.window.key.MOD_SHIFT:
                 dir = -1
@@ -182,15 +162,15 @@ class Window(pyglet.window.Window):
         elif symbol == pyglet.window.key.ENTER:
             for widget in self.widgets:
                 if self.widgets.index(widget) <= 2:
-                    hsvLimitLower[self.widgets.index(widget)] = np.uint8(widget.document.text)
+                    vl.hsvLimitLower[self.widgets.index(widget)] = np.uint8(widget.document.text)
                 elif 2 < self.widgets.index(widget) <= 5:
-                    hsvLimitUpper[self.widgets.index(widget) - 3] = np.uint8(widget.document.text)
+                    vl.hsvLimitUpper[self.widgets.index(widget) - 3] = np.uint8(widget.document.text)
             i = 6
             for prop in visionProperties:
                 if isinstance(self.widgets[i], Button):
                     break
                 else:
-                    visionProperties[prop] = int(self.widgets[i].document.text)
+                    vl.visionProperties[prop] = int(self.widgets[i].document.text)
                     i += 1
 #            print(hsvLimitLower)
 #            print(hsvLimitUpper)
@@ -211,68 +191,6 @@ class Window(pyglet.window.Window):
             self.focus.caret.position = len(self.focus.document.text)
 
 
-def filterContours(contours):
-    output = []
-    global visionProperties
-    for contour in contours:
-        x, y, w, h = cv.boundingRect(contour)
-        if (w < visionProperties["widthMin"] or w > visionProperties["widthMax"]):
-            continue
-        if (h < visionProperties["heightMin"] or h > visionProperties["heightMax"]):
-            continue
-        area = cv.contourArea(contour)
-        if area < visionProperties["areaMin"]:
-            continue
-        hull = cv.convexHull(contour)
-        solid = 100 * area / cv.contourArea(hull)
-        if (solid < visionProperties["solidityMin"]) or (solid > visionProperties["solidityMax"]):
-            continue
-        if (len(contour) < visionProperties["vertexMin"] or len(contour) > visionProperties["vertexMax"]):
-            continue
-        ratio = (float)(w) / h
-        if (ratio < visionProperties["ratioMin"] or ratio > visionProperties["ratioMax"]):
-            continue
-        output.append(contour)
-    return output
-
-
-def main(dt):
-    global vs
-    global hsvLimitLower
-    global hsvLimitUpper
-    retval, frame = vs.read()
-    # frame = cv.imread(r"9ft.png")
-    cv.imshow('frame', frame)
-    kernel = np.ones((5, 5), np.uint8)
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    mask = cv.inRange(hsv, hsvLimitLower, hsvLimitUpper)
-    cv.imshow('mask', mask)
-    contours, hierarchy = cv.findContours(mask, 1, 2)
-#   cnt = contours[0]
-    backdrop = np.zeros((480, 640, 3))  # Backdrop to display the contours
-
-    frame_and_target_contour = frame
-    filteredContours = filterContours(contours)
-    try:
-        cnt = filteredContours[0]
-        M = cv.moments(cnt)
-        cx = int(M['m10'] / M['m00'])
-        cy = int(M['m01'] / M['m00'])
-        print(cx)
-        print(cy)
-    except:
-        pass
-    try:
-        x, y, w, h = cv.boundingRect(filteredContours[0])
-        rect = cv.rectangle(backdrop, (x, y),
-                            ((x + w), (y + h)), (255, 0, 0), 10)
-        print(rect)
-    except:
-        pass
-    cv.imshow('processed', cv.drawContours(backdrop,
-              filteredContours, -1, (0, 255, 0), 3))
-
-
 window = Window(resizable=True)
-pyglet.clock.schedule(main)
+pyglet.clock.schedule(vl.isoTarget)
 pyglet.app.run()
