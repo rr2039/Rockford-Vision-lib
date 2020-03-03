e94fc5a48241db85e99b02f1d38cf750e740e0a6
Dawson Schraiber <dschraiber1@gmail.com>
2020-03-03 18:32:00 +0000

    Cleaned up syntax

--- testFiles/visionLibrary.py
+++ testFiles/visionLibrary.py
@@ -21,48 +21,52 @@ visionProperties = {"areaMin": 3,
 def distanceToCamera(knownWidth, focalLength, perWidth):
     return (knownWidth * focalLength) / perWidth
 
+
 def filterContours(contours):
     output = []
     global visionProperties
     for contour in contours:
         x, y, w, h = cv.boundingRect(contour)
-        if (w < visionProperties["widthMin"] or w > visionProperties["widthMax"]):
+        if (w < visionProperties["widthMin"] or
+                w > visionProperties["widthMax"]):
             continue
-        if (h < visionProperties["heightMin"] or h > visionProperties["heightMax"]):
+        if (h < visionProperties["heightMin"] or
+                h > visionProperties["heightMax"]):
             continue
         area = cv.contourArea(contour)
         if area < visionProperties["areaMin"]:
             continue
         hull = cv.convexHull(contour)
         solid = 100 * area / cv.contourArea(hull)
-        if (solid < visionProperties["solidityMin"]) or (solid > visionProperties["solidityMax"]):
+        if ((solid < visionProperties["solidityMin"]) or
+                (solid > visionProperties["solidityMax"])):
             continue
-        if (len(contour) < visionProperties["vertexMin"] or len(contour) > visionProperties["vertexMax"]):
+        if (len(contour) < visionProperties["vertexMin"] or
+                len(contour) > visionProperties["vertexMax"]):
             continue
         ratio = (float)(w) / h
-        if (ratio < visionProperties["ratioMin"] or ratio > visionProperties["ratioMax"]):
+        if (ratio < visionProperties["ratioMin"] or
+                ratio > visionProperties["ratioMax"]):
             continue
         output.append(contour)
     return output
 
 
 def isoTarget(dt):
- #   global vs
+    global vs
     global hsvLimitLower
     global hsvLimitUpper
     global KNOWN_WIDTH
-    global KNOWN_DISTANCE
     retval, frame = vs.read()
     # frame = cv.imread(r"4ft2in.png")
     cv.imshow('frame', frame)
-    kernel = np.ones((5, 5), np.uint8)
+    # kernel = np.ones((5, 5), np.uint8)
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     mask = cv.inRange(hsv, hsvLimitLower, hsvLimitUpper)
     cv.imshow('mask', mask)
     contours, hierarchy = cv.findContours(mask, 1, 2)
     backdrop = np.zeros((480, 640, 3))  # Backdrop to display the contours
-
-    frame_and_target_contour = frame
+    # frame_and_target_contour = frame
     filteredContours = filterContours(contours)
     try:
         cnt = filteredContours[0]
@@ -85,9 +89,10 @@ def isoTarget(dt):
     cv.imshow('processed', cv.drawContours(backdrop,
               filteredContours, -1, (0, 255, 0), 3))
 
+
 if __name__ == '__main__':
     while True:
         isoTarget(None)
-        if cv.waitKey(1) == ord('\x1b'): # Escape Keycode
+        if cv.waitKey(1) == ord('\x1b'):  # Escape Keycode
             break
     cv.destroyAllWindows()
